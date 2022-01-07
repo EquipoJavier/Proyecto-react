067b4886f482371f911a42d5c0cf7b38b284fd70
Fersanseg <82152264+Fersanseg@users.noreply.github.com>
2022-01-07 19:18:35 +0100

    popup hecho (sin animaciones

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,25 +4,28 @@ import PopupElem from './Popup-elem';
 /* Idealmente el "json" que le pases a Popup para que los presente en pantalla lo deberías pasar a través de la página que lo vaya a usar
 (Gastronomía le pasa "platos", Transportes le pasa "medios de transporte", etc). No sé cómo hacer eso exactamente, así que de momento a modo
 de prueba, voy a poner los datos de los platos aquí, a la espera de saber cómo pasarle el "json" aquí para dejar Popup como un componetne genérico.*/
+
+/* Las imágenes no están cargadas desde la carpeta '/src/Recursos/img' sino desde: '/public/imagenesTest'. Si luego descubrieramos una mejor forma de 
+importar imágenes, habría que refactorizar el nombre de esa carpeta (la de /public/imagenesTest) y cambiar el 'src' de la imagen en Popup-elem.js */
 const dishes = [
     {
         name: "Cocido",
-        img: '../Recursos/img/gastr-cocido.jpg',
+        img: 'gastr-cocido.jpg',
         descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Callos",
-        img: '../Recursos/img/gastr-cocido.jpg',
+        img: 'gastr-callos.jpg',
         descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Calamares",
-        img: '../Recursos/img/gastr-cocido.jpg',
+        img: 'gastr-bocadilloCalamares.jpg',
         descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Churros",
-        img: '../Recursos/img/gastr-cocido.jpg',
+        img: 'gastr-churros.jpg',
         descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
 ]
@@ -30,17 +33,13 @@ const dishes = [
 export default function Popup() {
     return (
         <div className="popup">
-            {/* {dishes.map(function(dish) {
+            {dishes.map(function(dish) {
                 return (
-                    <div key={dish.name}>
+                    <div className="popup__elem" key={dish.name}>
                         <PopupElem dish={dish}/>
                     </div>
                 )
-            })} */}
-            <div className="popup__elem">test</div>
-            <div className="popup__elem">test</div>
-            <div className="popup__elem">test</div>
-            <div className="popup__elem">test</div>
+            })}
         </div>
     )
 }
