9267ae7421a55a2ad632693d7c85e6488570d831
Sima <panchosima@gmail.com>
2022-01-18 13:52:54 +0100

    Fixed some problems with Menu and added css property sticky

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -12,27 +12,42 @@ export default function Menu(props) {
           <li className={location.pathname === "/" ? "menu__item index" : 'menu__item' }>
               <Link to="/" onClick={function() {
                 getHeading("Madrid", "Descubre todos sus secretos");
-              }}>Index</Link>
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+              })}}>Index</Link>
           </li>
           <li className={location.pathname === "/transportes" ? " menu__item transportes" : 'menu__item' }>
               <Link to="/transportes" onClick={function() {
                 getHeading("Transportes", "");
-              }}>Transportes</Link>
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+              })}}>Transportes</Link>
           </li>
           <li className={location.pathname === "/ocio" ? " menu__item ocio" : 'menu__item' }>
               <Link to="/ocio" onClick={function() {
                 getHeading("Ocio", "");
-              }}>Ocio</Link>
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+              })}}>Ocio</Link>
           </li>
           <li className={location.pathname === "/gastronomia" ? " menu__item gastronomia" : 'menu__item' }>
               <Link to="/gastronomia" onClick={function() {
                 getHeading("Gastronomia", "Un sabor inigualable");
-              }}>Gastronomia</Link>
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+              })}}>Gastronomia</Link>
           </li>
           <li className={location.pathname === "/cultura" ? " menu__item cultura" : 'menu__item' }>
               <Link to="/cultura" onClick={function() {
                 getHeading("Cultura", "");
-              }}>Cultura</Link>
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+              })}}>Cultura</Link>
           </li>
         </ul>
       </nav>
