039b52af944a726f12f8f9c06a4cccb45f0b303c
Sima <panchosima@gmail.com>
2022-01-18 13:27:08 +0100

    Deleted innecesary navs

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -10,39 +10,29 @@ export default function Menu(props) {
       <nav className="menu">
         <ul className="menu__list">
           <li className={location.pathname === "/" ? "menu__item index" : 'menu__item' }>
-            <nav className="menu__item--text">
               <Link to="/" onClick={function() {
                 getHeading("Madrid", "Descubre todos sus secretos");
               }}>Index</Link>
-            </nav>
           </li>
           <li className={location.pathname === "/transportes" ? " menu__item transportes" : 'menu__item' }>
-            <nav>
               <Link to="/transportes" onClick={function() {
                 getHeading("Transportes", "");
               }}>Transportes</Link>
-            </nav>
           </li>
           <li className={location.pathname === "/ocio" ? " menu__item ocio" : 'menu__item' }>
-            <nav>
               <Link to="/ocio" onClick={function() {
                 getHeading("Ocio", "");
               }}>Ocio</Link>
-            </nav>
           </li>
           <li className={location.pathname === "/gastronomia" ? " menu__item gastronomia" : 'menu__item' }>
-            <nav>
               <Link to="/gastronomia" onClick={function() {
                 getHeading("Gastronomia", "Un sabor inigualable");
               }}>Gastronomia</Link>
-            </nav>
           </li>
           <li className={location.pathname === "/cultura" ? " menu__item cultura" : 'menu__item' }>
-            <nav>
               <Link to="/cultura" onClick={function() {
                 getHeading("Cultura", "");
               }}>Cultura</Link>
-            </nav>
           </li>
         </ul>
       </nav>
