c79296c4747acae5ff227f0b1185a48773a6fdfe
Sima <panchosima@gmail.com>
2022-01-19 14:20:25 +0100

    Added LinkTransport Component and adding more styles

--- src/components/Transportes/Section_Location/SectionLocation.js
+++ src/components/Transportes/Section_Location/SectionLocation.js
@@ -2,6 +2,7 @@ import ListTransport from "./ListTransport/ListTransport";
 import Map from "./Map/Map";
 import "./SectionLocation.scss";
 import "../../../sass/base/animations.scss";
+import LinkTransport from "./LinkTransport/LinkTransport";
 
 
 export default function SectionLocation(){
@@ -13,6 +14,7 @@ export default function SectionLocation(){
             <hr width="400" />
             <div className="section__location--flex">
                 <Map />
+                <LinkTransport />
                 <ListTransport />
             </div>
         </section>
