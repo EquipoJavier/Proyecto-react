1b5d32c7970fa8a4587949c7828acc4a821103c2
Tania <tania.sdj@hotmail.com>
2022-01-19 14:11:37 +0100

    finished slider

--- src/components/Ocio/SecondSection/Slider/Slider.js
+++ src/components/Ocio/SecondSection/Slider/Slider.js
@@ -3,27 +3,19 @@ import slider1 from "../../../Recursos/img/carrousel1.jpg";
 import slider2 from "../../../Recursos/img/carrousel2.jpg";
 import slider3 from "../../../Recursos/img/carrousel3.png";
 import slider4 from "../../../Recursos/img/carrousel4.png";
+import slider5 from "../../../Recursos/img/carrousel5.png";
 
-//he cambiado el nombre="carrousel" por slider
+
 export default function Slider(){
     return(
-    //     <>
-    //     <div className="slider">
-    //     <div className="slider--img">
-    //         <img className="img_slider" src={slider1} alt="" height="400" width="800" />
-    //         <img className="img_slider" src={slider2} alt="" height="400" width="800" />
-    //         <img className="img_slider" src={slider3} alt="" height="400" width="800" />
-    //         <img className="img_slider" src={slider4} alt="" height="400" width="800" />
-    //     </div>
-    // </div>
-    // </>
 
-<figure class="icon-cards mt-3">
-  <div class="icon-cards__content">
-   <img className="icon-cards__item d-flex align-items-center justify-content-center" src={slider1} />
-   <img className="icon-cards__item d-flex align-items-center justify-content-center" src={slider2} />
-   <img className="icon-cards__item d-flex align-items-center justify-content-center" src={slider3} />
-   <img className="icon-cards__item d-flex align-items-center justify-content-center" src={slider4} />
+<figure class="slider">
+  <div class="slider__content">
+   <img className="slider__item" src={slider1} alt=""/>
+   <img className="slider__item" src={slider2} alt=""/>
+   <img className="slider__item" src={slider3} alt=""/>
+   <img className="slider__item" src={slider4} alt=""/>
+   <img className="slider__item" src={slider5} alt=""/>
   </div>
 </figure>
     )
