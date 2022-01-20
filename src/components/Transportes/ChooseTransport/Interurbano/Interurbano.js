a0bef9f2534b9c8500a118bbdbd4482c622b647d
Sima <panchosima@gmail.com>
2022-01-20 13:46:35 +0100

    Update Interurbano.js

--- src/components/Transportes/ChooseTransport/Interurbano/Interurbano.js
+++ src/components/Transportes/ChooseTransport/Interurbano/Interurbano.js
@@ -1,12 +1,10 @@
 export default function Interurbano() {
   return (
     <>
-      <a href="#popup_inter">
-        <div className="  choose__transport_options--option">
-          <img src="../Recursos/img/interurbanos_cm_xl.gif" alt="" />
-          <h2>Líneas Inter</h2>
-        </div>
-      </a>
+      <div className="  choose__transport_options--option">
+        <img src="../Recursos/img/interurbanos_cm_xl.gif" alt="" />
+        <h2>Líneas Inter</h2>
+      </div>
     </>
   );
 }
