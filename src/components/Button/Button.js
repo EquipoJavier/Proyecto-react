82e922e941244acdfdeb5d69a8f21b1f12e0a64d
Fersanseg <82152264+Fersanseg@users.noreply.github.com>
2022-01-27 14:10:45 +0100

    button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import './Button.scss';
 /*
-Props que hay que pasarle al botón (tienen que pasarse con estos nombres):
+Props que hay que pasarle al botón (tienen que pasarse con estos nombres).
+Las propiedades son opcionales: cuando incluyas este componente, al llamarlo, puedes pasarle estas propiedades o no
+Por ejemplo, si no le pasas una url, el botón no redirigirá a ningún lado. Si no le pasas clase de hover, los estilos no cambiarán, etc.
     url: el enlace que se va a abrir
     title: el texto que saldrá en el botón
     hoverClass: los estilos que se añadirán al hacer hover sobre el botón (ver Button.scss)
-*/
+    onClick: Función que se ejecutará al hacer click en el botón (si no se pasa esta prop al incluir el botón, simplemente no hará)
+    */
 export default function Button(props) {
     /*
     Al poner el ratón sobre el botón (el evento onMouseEnter) y quitarlo (onMouseLeave), se llama a toggleHover.
@@ -21,8 +24,7 @@ export default function Button(props) {
     const cssClass = "button " + (hovered ? props.hoverClass : "" );
 
     return (
-        // <a href={props.url} className={cssClass} style={hoverStyle} onMouseEnter={toggleHover} onMouseLeave={toggleHover} target="_blank" rel="noreferrer">
-        <a href={props.url} className={cssClass} onMouseEnter={toggleHover} onMouseLeave={toggleHover} target="_blank" rel="noreferrer">
+        <a href={props.url} className={cssClass} onClick={props.onClick} onMouseEnter={toggleHover} onMouseLeave={toggleHover} target="_blank" rel="noreferrer">
             {props.title}
         </a>
     )
