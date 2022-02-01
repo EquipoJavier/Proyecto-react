bfcb72913b211410aa2c5c564e6cd6f8515394f3
Sima <panchosima@gmail.com>
2022-02-01 09:57:02 +0100

    Continuing to finish the Popup

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ function App() {
 
   const pathname = useLocation().pathname; //obtengo la ruta acutal p.ejem("/transportes")
   // const url = `http://localhost:3001${pathname === "/" ? "/index" : pathname}`;
-  const url = "http://localhost:3001/data";
+  const url = "http://st:3001/data";
   //le indico la ruta con el recurso a solicitar y en el caso de que pathname hubiera sido "/" (referido a index), a la url le paso /index
 
   //Se corre este efecto cada vez que cambie la url (es decir, queramos solicitar otro recurso)
--- src/components/Transportes/ChooseTransport/Popup/Popup.js
+++ src/components/Transportes/ChooseTransport/Popup/Popup.js
@@ -1,51 +1,52 @@
-import metro from "../../../Recursos/img/metro_xl.gif";
-import emt from "../../../Recursos/img/emt_xl.gif";
-import inter from "../../../Recursos/img/interurbanos_cm_xl.gif";
-import ligero from "../../../Recursos/img/metroligero_xl.gif";
-import cercanias from "../../../Recursos/img/cercanias_xl.gif";
 import loading from "../../../Recursos/img/loading.gif";
 import "./Popup.scss";
+import PopupMenu from "./PopupMenu/PopupMenu";
 
-
-export default function Popup({ popupInfo, done, types, show, setShow }) {
-
-  
-  if(done){
+export default function Popup({
+  popupInfo,
+  done,
+  setTypes,
+  types,
+  show,
+  setShow,
+}) {
+  if (done) {
     var info = popupInfo[types];
   }
 
   return (
-    <div className={ show ? "popup_transportes": "hidden"}>
-      <div className="popup_transportes--btn" onClick={() => {
-        setShow(false);
-        window.onscroll=function(){};
-      }}>
+    <div className={show ? "popup_transportes" : "hidden"}>
+      <div
+        className="popup_transportes--btn"
+        onClick={() => {
+          setShow(false);
+          window.onscroll = function () {};
+        }}
+      >
         <i className="material-icons">close</i>
       </div>
-      <div className="popup_transportes--options" >
-        <img src={metro} alt="" />
-        <img src={emt} alt="" />
-        <img src={inter} alt="" />
-        <img src={ligero} alt="" />
-        <img src={cercanias} alt="" />
+      <div className="popup_transportes--options">
+        <PopupMenu setTypes={setTypes} type={"metro"} />
+        <PopupMenu setTypes={setTypes} type={"emt"} />
+        <PopupMenu setTypes={setTypes} type={"inter"} />
+        <PopupMenu setTypes={setTypes} type={"ligero"} />
+        <PopupMenu setTypes={setTypes} type={"cercanias"} />
       </div>
 
-      { info ? <><div className="popup_transportes-aside">
-        <h1 className="popup_transportes-aside--h1">{info.name}</h1>
-        <aside className="popup_transportes-aside--text">
-          <p className="popup_transportes-aside--text-p">
-            {info.text}
-          </p>
-        </aside>
-      </div></> 
-      : 
-      (
-        <img style={{
-          width: "70px"
-        }} src={loading} alt="Loading..." />
-      )
-      }
-      
+      {info ? (
+        <>
+          <div className="popup_transportes-aside">
+            <h1 className="popup_transportes-aside--h1">{info.name}</h1>
+            <aside className="popup_transportes-aside--text">
+              <p className="popup_transportes-aside--text-p">{info.text}</p>
+            </aside>
+          </div>
+        </>
+      ) : (
+        <div className="popup_transportes--loading">
+          <img src={loading} alt="Loading..." />
+        </div>
+      )}
     </div>
   );
 }
