c8e01b94d1cfc0d4c02979132bd3fb81454cff9f
Tania <tania.sdj@hotmail.com>
2022-02-03 10:20:56 +0100

    starting add component material ui

--- src/components/Ocio/FourthSection/FourthSection.js
+++ src/components/Ocio/FourthSection/FourthSection.js
@@ -1,17 +1,17 @@
-import Routes from "./Routes/Routes";
+
 import "./FourthSection.scss";
+import CustomImageList from "./Routes/Routes";
+
 
 export default function FourthSection() {
     return (
         <>
             <section className="fourth__section">
                 <h2 className="fourth__section--title">DESCUBRE TUS RUTAS PARA DESCONECTAR</h2>
-                <div className="fourth__section--routes">
-               <Routes name="image1"/>
-               <Routes name="image2"/>
-               <Routes name="image3"/>
-               <Routes name="image4"/>
-               </div>
+              
+              <CustomImageList />
+               
+              
             </section>
         </>
     )
--- src/components/Ocio/FourthSection/Routes/Routes.js
+++ src/components/Ocio/FourthSection/Routes/Routes.js
@@ -1,52 +1,140 @@
 import "./Routes.scss";
-import landscape1 from "../../../Recursos/img/casa-campo.jpeg";
-import landscape2 from "../../../Recursos/img/silla-felipe.jpg";
-import landscape3 from "../../../Recursos/img/hayedo-de-montejo.jpg";
-import landscape4 from "../../../Recursos/img/Palacio_Real_de_Aranjuez_0.webp";
+
+// import ImageList from '../../../Recursos/img/casa-campo.jpeg';
+// import ImageListItem from '../../../Recursos/img/silla-felipe.jpg';
+// import ImageListItemBar from '../../../Recursos/img/hayedo-de-montejo.jpg';
+// import IconButton from '../../../Recursos/img/Palacio_Real_de_Aranjuez_0.webp';
+// import StarBorderIcon from '../../../Recursos/img/parque-sierra--guadarrama.jpg';
 
 
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
-const routes =[
-    {
-        name: "image1",
-        text: "Ruta Casa de Campo",
-        image: landscape1,
-        id:"1",
-
-    },
-
-    {
-        name: "image2",
-        text: "Ruta de la silla de Felipe II",
-        image: landscape2,
-        id:"2",
-    },
-
-    {
-        name: "image3",
-        text: "Ruta de Hayedo de Montejo",
-        image: landscape3,
-        id:"3",
-    },
-
-    {
-        name: "image4",
-        text: "Palacio Aranjuez",
-        image: landscape4,
-        id:"4",
-    }
-]
-
-export default function FourthSection(props) {
-    return (
-        <>
-          {routes.map(function (item) {
-              return <>
-                <div className="routes">
-                       <>{item.name === props.name ? <img key={item.id} className={`routes__img routes__img--${item.id}`} src={item.image} alt="" />: null}</>
-                      <>{item.name === props.name ? <h4 key={item.id+"h4"} className={`routes__text routes__text--${item.id}`}>{item.text}</h4>: null}</>
-                </div></> })};
-        </>
-         
-    )
+function srcset(image, width, height, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${width * cols}&h=${
+      height * rows 
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
 }
+
+export default function CustomImageList() {
+  return (
+    <ImageList
+      sx={{
+        width: 500,
+        height: 450,
+        // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
+        transform: 'translateZ(0)',
+      }}
+      rowHeight={200}
+      gap={1}
+    >
+      {itemData.map((item) => {
+        const cols = item.featured ? 2 : 1;
+        const rows = item.featured ? 2 : 1;
+
+        return (
+          <ImageListItem key={item.img} cols={cols} rows={rows}>
+            <img
+              {...srcset(item.img, 250, 200, rows, cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              sx={{
+                background:
+                  'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                  'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+              }}
+              title={item.title}
+              position="top"
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'white' }}
+                  aria-label={`star ${item.title}`}
+                >
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+            />
+          </ImageListItem>
+        );
+      })}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+    author: '@bkristastucchio',
+    featured: true,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+    author: '@rollelflex_graphy726',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+    author: '@helloimnik',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+    author: '@nolanissac',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+    author: '@hjrc33',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+    author: '@arwinneil',
+    featured: true,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+    author: '@tjdragotta',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+    author: '@katie_wasserman',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+    author: '@southside_customs',
+  },
+];
+
+
+
+
